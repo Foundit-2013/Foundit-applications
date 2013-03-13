f4bc9d1d5696aa71013dbfa9376680d14bf00946
Parnit Pooni <parnit_p@Parnits-MacBook-Pro.local>
2013-03-13 18:28:51 -0400

    testing submission

--- FoundItMobile/src/com/example/foundit/FullscreenImage.java
+++ FoundItMobile/src/com/example/foundit/FullscreenImage.java
@@ -23,7 +23,7 @@ public class FullscreenImage extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.//
+		// Inflate the menu; this adds items to the action bar if it is present.///
 		getMenuInflater().inflate(R.menu.activity_fullscreen_image, menu);
 		return true;
 	}
